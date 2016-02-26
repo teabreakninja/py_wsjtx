b2ac7881148aad690e92f04d51d426eb16293a05
teabreakninja <teabreakninja@github.com>
2016-02-26 17:58:57 +0000

    Qso_logged working now, also changes to date/time decode

--- wsjtx/Qso_Logged.py
+++ wsjtx/Qso_Logged.py
@@ -20,32 +20,41 @@ class Qso_Logged:
         string_length, self.id_key = myutils.get_utf8_string(data)
         # print("  id_key: {} (len:{})".format(self.id_key, string_length))
         tmp = 4 + string_length
-        
+
         self.date_time = myutils.get_datetime(data[tmp:])
-        tmp += 4
-        
-        len, self.dx_call = myutils.get_utf8_string(data[tmp:])
-        tmp += 4 + len
-        
-        len, self.dx_grid = myutils.get_utf8_string(data[tmp:])
-        tmp += 4 + len
-        
-        self.dial_freq = myutils.get_uint32(data[tmp:])
-        tmp += 4
-        
-        len, self.mode = myutils.get_utf8_string(data[tmp:])
-        tmp += 4 + len
-        
-        len, self.report_send = myutils.get_utf8_string(data[tmp:])
-        tmp += 4 + len
-        
-        len, self.report_recv = myutils.get_utf8_string(data[tmp:])
-        tmp += 4 + len
-        
-        len, self.tx_power = myutils.get_utf8_string(data[tmp:])
-        tmp += 4 + len
-        
-        len, self.name = myutils.get_utf8_string(data[tmp:])
+        tmp += 13
+        # print("datetime:{}".format(self.date_time))
+
+        string_length, self.dx_call = myutils.get_utf8_string(data[tmp:])
+        tmp += 4 + string_length
+        # print("DX_Call:"+self.dx_call)
+
+        string_length, self.dx_grid = myutils.get_utf8_string(data[tmp:])
+        tmp += 4 + string_length
+        # print("dx_grid:"+self.dx_grid)
+
+        self.dial_freq = myutils.get_int64(data[tmp:])
+        tmp += 8
+        # print("Dial:{}".format(self.dial_freq))
+
+        string_length, self.mode = myutils.get_utf8_string(data[tmp:])
+        tmp += 4 + string_length
+        # print("mode:"+self.mode)
+
+        string_length, self.report_sent = myutils.get_utf8_string(data[tmp:])
+        tmp += 4 + string_length
+        # print("report_sent:"+self.report_sent)
+
+        string_length, self.report_recv = myutils.get_utf8_string(data[tmp:])
+        tmp += 4 + string_length
+        # print("report_recv:"+self.report_recv)
+
+        string_length, self.tx_power = myutils.get_utf8_string(data[tmp:])
+        tmp += 4 + string_length
+        # print("rx_power:"+self.tx_power)
+
+        string_length, self.name = myutils.get_utf8_string(data[tmp:])
+        # print("name:"+self.name)
 
     def do_print(self):
         print("{} logged QSO with {} ({}) on {} using {}. Send {} received {}".format(
