6eebcd96eb6fa4c4df0ff79b8ee0b40161a82cb9
teabreakninja <teabreakninja@github.com>
2016-12-31 18:34:36 +0000

    Change in Heartbeat object in wsjtx v1.7

--- wsjtx/Heartbeat.py
+++ wsjtx/Heartbeat.py
@@ -10,7 +10,8 @@ class Heartbeat:
 
     def __init__(self, data):
         string_length, self.id_key = myutils.get_utf8_string(data)
+        tmp = 4 + string_length
 
-        self.max_schema = struct.unpack(">L", data[4 + string_length:])[0]
+        self.max_schema = myutils.get_uint32(data[tmp:])
         # myutils.debug_hex(data[4+self.string_length:])
         # print("[*] max_schema: {}".format(self.max_schema))
